123b6cd93251da99b8c2dcbe892244262d468130
Karan Goel <karan@goel.im>
2014-06-05 07:45:23 -0700

    add insertion sort

---
+++ InsertionSort.playground/section-1.swift
@@ -0,0 +1,31 @@
+// Insertion Sort
+// Karan Goel
+
+var randomNumbers = [42, 12, 88, 62, 63, 56, 1, 77, 88, 97, 97, 20, 45, 91, 62, 2, 15, 31, 59, 5]
+
+func insertionSort(nums: Int[]) {
+    
+    for i in 1..nums.count {
+        
+        var x = nums[i]
+        
+        var j = i
+        
+        while j > 0 && nums[j-1] > x {
+            
+            nums[j] = nums[j-1]
+            
+            j--
+            
+        }
+        
+        nums[j] = x
+        
+    }
+    
+}
+
+// pass by reference
+insertionSort(randomNumbers)
+
+println(randomNumbers)
