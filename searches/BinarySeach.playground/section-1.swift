0eed3141a77268839695d371515ce8f69c4c519c
Piper Chester <piperchester@gmail.com>
2014-06-12 11:09:58 -0400

    Implement good ol' binary search

---
+++ searches/BinarySeach.playground/section-1.swift
@@ -0,0 +1,33 @@
+// Binary Search
+// Piper Chester
+
+func binarySearch(numbers:Int[], target: Int){
+    // Initialize clamps
+    var left = numbers.startIndex
+    var right = numbers.endIndex
+    var midpoint = numbers.count / 2
+    var iterations = 0
+
+    for index in left...right {
+        iterations += 1
+        if midpoint < target {
+            println("Low: \(midpoint) < \(target)")
+            left = midpoint
+            midpoint = (right + left) / 2
+        } else if midpoint > target {
+            println("High: \(midpoint) > \(target)")
+            right = midpoint
+            midpoint = (right + left) / 2
+        } else {
+            print("Target, \(target) acquired after \(iterations) iterations")
+            break
+        }
+    }
+}
+
+var numbers:Int[] = []
+for i in 0..1000 {
+    numbers.insert(i, atIndex: i)
+}
+
+binarySearch(numbers, 999)
